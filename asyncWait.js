f3bba2aae8a5c18da233ae27d348a33c5cb0b994
Pedro Dambrosio <pedrodam99@gmail.com>
2022-04-12 10:50:05 -0300

    Async/ Await

---
+++ asyncWait.js
@@ -0,0 +1,35 @@
+const http = require('http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+    
+            res.on('data', dados => {
+                resultado += dados
+            })
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+//RECURSO DO ES8
+//simplifica o uso de promise 
+//só pode usar o await dentro de um escopo onde o async marca uma variavel
+let obterAlunos = async () => {
+    const ta = await getTurma('A')
+    const tb = await getTurma('B')
+    const tc = await getTurma('C')
+    return [].concat(ta, tb, tc)
+}
+//retorna uma AsyncFN
+obterAlunos()
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes => console.log(nomes))
